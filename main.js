e9e51beabd4a034eb7a98578898aabebbea789df
NievesViri <105406390+NievesViri@users.noreply.github.com>
2022-10-25 20:40:32 -0500

    added code

--- main.js
+++ main.js
@@ -11,9 +11,24 @@ const rl = readline.createInterface({
 });
 
 
-const pigLatin = (word) => {
-
-  // Your code here
+const pigLatin = (str) => {
+// Your code here
+  let vowels = [ 'a', 'e', 'i', 'o', 'u'];
+  let newStr = "";
+ if (vowels.indexOf(str[0])> -1) {
+  newStr = str + "yay";
+  return newStr;
+ } else {
+  let firstMatch = str.match(/[aeiou]/g) || 0;
+  
+  let vowel = str.indexOf(firstMatch[0]);
+  newStr =
+  str.substring(vowel) +
+  str.substring(0, vowel) +
+  "ay";
+  return newStr;
+ }
+  
 
 }
 
